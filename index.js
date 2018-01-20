915b34b7cbe0e8b198066405cd1a32721df656b1
WooLNinesun <woolninesun@gmail.com>
2018-01-20 16:45:32 +0800

    before_render_post event to add uuid if post not have uuid

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 
 const HexoUuid = require('./lib/hexo-uuid');
 
-hexo.on('new', HexoUuid);
+hexo.on('new', HexoUuid.newPost);
+
+hexo.extend.filter.register('before_post_render', HexoUuid.before_renderPost);
