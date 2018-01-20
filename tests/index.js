4aa0c62cdc60c9ae6e23c25f9fae30e0a0fcba2a
WooLNinesun <woolninesun@gmail.com>
2018-01-20 19:21:09 +0800

    modify tests/index.js to test before_post_render function and change test-cover _mocha cmd path for win10

--- tests/index.js
+++ tests/index.js
@@ -8,9 +8,9 @@ const hexo = new Hexo(__dirname, {
   silent: true
 });
 
-hexo.on('new', HexoUuid);
+hexo.on('new', HexoUuid.newPost);
 
-describe('Post With User Pre-defined UUID Attribute', () => {
+describe('Post With User Pre-defined UUID Attribute on new post', () => {
 
   let post = {
     path: false,
@@ -32,7 +32,7 @@ tags:
 
 });
 
-describe('Post Without User Pre-defined UUID Attribute', () => {
+describe('Post Without User Pre-defined UUID Attribute on new post', () => {
 
   let post = {
     path: './tmp/test.md',
@@ -48,6 +48,81 @@ tags:
 
   it('Post should have uuid', () => {
     let uuidPresence = /uuid: .{36}\n/.test(post.content);
+    uuidPresence.should.equal(true);
+  });
+
+});
+
+hexo.extend.filter.register('before_post_render', HexoUuid.before_renderPost);
+
+describe('Post With User Pre-defined UUID Attribute before render', () => {
+
+  let post = {
+    layout:'post',
+    uuid: '7848d740-fd9a-11e7-ba72-ab02344a9ad8',
+    full_source: './tmp/test.md',
+    raw: `'---
+uuid: 7848d740-fd9a-11e7-ba72-ab02344a9ad8
+title: I love Hexo!
+date: 2016-05-20 16:20
+tags:
+---
+test
+    `
+  };
+
+  hexo.extend.filter.exec('before_post_render', post);
+
+  it('Post should have uuid', () => {
+    let uuidPresence = /uuid: .{36}\n/.test(post.raw);
+    uuidPresence.should.equal(true);
+  });
+
+});
+
+describe('Post Without User Pre-defined UUID Attribute before render', () => {
+
+  let post = {
+    layout:'post',
+    full_source: './tmp/test.md',
+    raw: `'---
+title: I love Hexo!
+date: 2016-05-20 16:20
+tags:
+---
+test
+    `
+  };
+
+  hexo.extend.filter.exec('before_post_render', post);
+
+  it('Post should have uuid', () => {
+    let uuidPresence = /uuid: .{36}\n/.test(post.raw);
+    uuidPresence.should.equal(true);
+  });
+
+});
+
+describe('Post With User Pre-defined UUID Attribute but blank before render', () => {
+
+  let post = {
+    uuid: '',
+    layout:'post',
+    full_source: './tmp/test.md',
+    raw: `'---
+uuid:
+title: I love Hexo!
+date: 2016-05-20 16:20
+tags:
+---
+test
+    `
+  };
+
+  hexo.extend.filter.exec('before_post_render', post);
+
+  it('Post should have uuid', () => {
+    let uuidPresence = /uuid: .{36}\n/.test(post.raw);
     uuidPresence.should.equal(true);
   });
 
